16f77b153815a4a708f31cb253e6cba6530c8cf5
Karen Valentim <karenvalentim54@gmail.com>
2025-08-06 20:37:58 -0300

    Primeiro componente: Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Footer from './footer.jsx';
 
 function App() {
   
 
   return (
     <>
-     
+      <Footer />
     </>
   )
 }
---
+++ src/footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="text-center py-3">
+    <p class="mb-0">&copy; 2025 Brigs Confeitaria. Feito com carinho.</p>
+  </footer>
+  )
+}
+
+export default Footer;
